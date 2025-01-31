51161093c3b729c5635d3d86f4566324e4e2311b
sonusharma85indore <sonu.sharma85indore@gmail.com>
2025-01-31 22:45:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,8 +7,14 @@ terraform {
   }
 }
 
-provider "aws" {
+
+/*provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
+}*/
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
 }
